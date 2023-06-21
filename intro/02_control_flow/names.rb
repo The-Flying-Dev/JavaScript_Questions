dda6750debc0c2a0fe3b1a4e7d7556e6d53cd4ed
The-Flying-Dev <leandranm@outlook.com>
2023-06-21 12:55:30 +0200

    02

--- intro/02_control_flow/names.rb
+++ intro/02_control_flow/names.rb
@@ -6,3 +6,14 @@ name4 = "Melisandre"
 
 # Your code goes here
 
+avg_length = (name1.length + name2.length + name3.length + name4.length) / 4
+
+p avg_length
+
+my_name = gets.chomp
+
+if my_name.length < avg_length 
+  p "#{my_name} is shorter than average"
+else  
+  p "#{my_name} is longer than average"
+end
