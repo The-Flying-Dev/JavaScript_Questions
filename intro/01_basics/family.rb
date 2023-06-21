19a6a944679f25c5dc9412b5862b9958ba3ed9fc
The-Flying-Dev <leandranm@outlook.com>
2023-06-21 12:50:49 +0200

    01

--- intro/01_basics/family.rb
+++ intro/01_basics/family.rb
@@ -6,3 +6,12 @@ mary = 16
 
 # Your code goes here
 
+age_sum = mom + dad + john + mary
+
+result = mom * dad / (john - mary)
+
+p "Sum of ages #{age_sum}"
+p "Result #{result}"
+
+p "Sum of ages #{age_sum}".reverse
+p "Result #{result}".reverse
