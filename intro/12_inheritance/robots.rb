b61aa1c5d58c34af743a3ce6faf47f52aac64d90
The-Flying-Dev <leandranm@outlook.com>
2023-06-24 00:28:20 +0200

    12

--- intro/12_inheritance/robots.rb
+++ intro/12_inheritance/robots.rb
@@ -1,15 +1,52 @@
 class Robot
   # Your code goes here
+  def weapon
+    puts "Canon"
+  end
 end
 
 class Ultron < Robot
   # Your code goes here
+  attr_reader :planet
+  def initialize(planet)
+    @planet = planet
+  end
+
+  def weapon 
+    puts "Air Shooter"
+  end
 end
 
 class MegaMan < Robot
   # Your code goes here
+  attr_reader :planet
+  def initialize(planet)
+    @planet = planet
+  end
+
+  def weapon 
+    super
+    puts "Bit Canon"
+  end
 end
 
 class Glados < Robot
   # Your code goes here
+  attr_reader :planet
+  def initialize(planet)
+    @planet = planet
+  end
+
+  def weapon 
+    puts "Triple Blade"
+  end
+
 end
+
+
+u = Ultron.new("Not Earth")
+u.weapon
+p u.planet
+
+m = MegaMan.new("New Earth")
+m.weapon
